78b82a2759a6098c96806030659747d84e3c24c0
Leandro Rauseo <leandro.rauseo@gmail.com>
2022-03-20 17:16:53 -0300

    adjust on readme file

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## springboot-keycloak-samle
     Example how to integrate Springboot with keycloak
+### keycloak on docker
+```
+ $ docker run -p 8080:8080 --name keycloak -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:17.0.0 start-dev
+```
 # Keycloak Console config
  - Realm: myrealm
  - client: myapp
--- src/main/java/com/lrrauseo/springbootkeycloak/HelloController.java
+++ src/main/java/com/lrrauseo/springbootkeycloak/HelloController.java
@@ -1,7 +1,6 @@
 package com.lrrauseo.springbootkeycloak;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-  // @RequestMapping(path = "/v1/health", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
   @GetMapping
   public ResponseEntity<String> hello() {
     return ResponseEntity.ok().body("hello world");
